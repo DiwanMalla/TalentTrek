4047d0b9bb2526b35e551cdda6e475e412007f4b
Diwan Malla <diwanmalla@192-168-1-3.tpgi.com.au>
2024-09-09 20:29:11 +1000

    Done router setup and create first landing page

---
+++ frontend/src/Components/Screen/LandingScreen.tsx
@@ -0,0 +1,13 @@
+const LandingScreen = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div id="container">
+        <div id="Title">
+          Find <span>Dream Internship</span> Program
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingScreen;
